052cf367d397cb0e8e1113652f28ead613c348c2
CorneliusNgure <kingcornelius07@gmail.com>
2024-08-13 23:07:02 -0400

    Task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,16 +1,16 @@
-function updateStudentGradeByCity(students, city, newGrades) {
+export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
-    .filter(student => student.location === city)
-    .map(student => {
-      const gradeEntry = newGrades.find(entry => entry.studentId === student.id);
-            
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeEntry = newGrades.find((entry) => entry.studentId === student.id);
+
       const grade = gradeEntry ? gradeEntry.grade : 'N/A';
-            
+
       return {
         id: student.id,
         firstName: student.firstName,
         location: student.location,
-        grade: grade
+        grade,
       };
     });
 }
