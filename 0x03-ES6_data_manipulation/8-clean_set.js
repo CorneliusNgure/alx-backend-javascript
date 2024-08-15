e065c09b7204b52dd6008ca4bfe4113f3518039c
CorneliusNgure <kingcornelius07@gmail.com>
2024-08-14 20:35:26 -0400

    Task 8

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,11 @@
+export default function cleanSet(set, startString) {
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+  const result = Array.from(set)
+    .filter(value => typeof value == 'string' && value.startsWith(startString))
+    .map(value => value.slice(startString.length))
+    .join('-');
+
+  return result;
+}
