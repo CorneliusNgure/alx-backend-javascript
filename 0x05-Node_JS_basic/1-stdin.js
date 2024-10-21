cca4b17b7d2efef5db0bbc54c4b47e581c8dcac5
CorneliusNgure <kingcornelius07@gmail.com>
2024-10-21 18:31:02 -0400

    Using process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,16 @@
 console.log('Welcome to Holberton School, what is your name?');
 
-// Listen for data (user input) on stdin
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim(); // Get the input and trim any extra spaces/newlines
-  console.log(`Your name is: ${name}`);
-
-  // After displaying the name, exit the process gracefully
-  process.exit();
-});
-
-// Handle the process exit event
-process.on('exit', () => {
-  console.log('This important software is now closing');
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
